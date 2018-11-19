41f77c92bafe4a125d0ba04858287afaf8300744
nampdn <nampdn@protonmail.com>
2018-11-19 07:38:48 +0700

    feat(hukk): add listenning server on POST method

--- src/hukk.ts
+++ src/hukk.ts
@@ -1,37 +1,63 @@
 import {createServer, Server} from 'http';
 
 export interface HukkIncomingHandle {
-  (data: any) : void
+  (data: any): void;
 }
 
 export interface HukkConfig {
-  endpoint: string
-  handle: HukkIncomingHandle
+  endpoint: string;
+  handle: HukkIncomingHandle;
 }
 
 export class Hukk {
-  port: number
-  hooks: HukkConfig[]
-  server: Server
+  static DEFAULT_PORT: number = 5678;
+  port: number;
+  hooks: HukkConfig[];
+  server: Server;
+
   constructor() {
-    this.port = 3000;
+    this.port = Hukk.DEFAULT_PORT;
     this.hooks = [];
   }
 
   register(hook: HukkConfig) {
-    this.hooks.push(hook)
+    this.hooks.push(hook);
   }
 
-  listen(port) {
+  listen(port, callback?: () => void) {
+    this.port = port || Hukk.DEFAULT_PORT;
     this.server = createServer(this._handleRequest);
-    this.server.listen('localhost', port || 3000, () => {});
+    this.server.listen(this.port, 'localhost', () => {
+      this._onStart();
+      callback && callback();
+    });
   }
 
-  _onStart() {
-    console.log('Hukk is running!');
+  close(callback: () => void) {
+    if (this.server && this.server.listening) {
+      this.server.close(callback);
+    }
   }
 
-  _handleRequest(req, res) {}
+  private _onStart() {
+    console.log(`Hukk is running on http://localhost:${this.port}`);
+  }
+
+  private _handleRequest(req, res) {
+    const {url, method} = req;
+    if (method === 'POST') {
+      let body = '';
+      req.on('data', data => {
+        body += data;
+      });
+      req.on('end', () => {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end('{"status": "received"}');
+      });
+    } else {
+      res.end(JSON.stringify({message: "Hukk doesn't support GET method!"}));
+    }
+  }
 }
 
 const hukk = new Hukk();
