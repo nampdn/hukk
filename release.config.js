0982e6a4fa54899689aa482d6e4211333e5a8123
nampdn <nampdn@protonmail.com>
2018-11-19 12:07:25 +0700

    ci(travis): add travis config
    add semantic-release for automatically release new version

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  prepare: ['@semantic-release/changelog', '@semantic-release/npm', '@semantic-release/git']
+};
