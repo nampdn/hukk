36b6b6797a8295d418606f8cd78b4ca205b96abd
nampdn <nampdn@protonmail.com>
2018-11-19 16:27:27 +0700

    feat(hukkup): add hukkup function
    hukkup help you send a webhook request to a Hukk server

---
+++ src/hukkup.ts
@@ -0,0 +1,43 @@
+import {request} from 'http';
+
+export interface HukkupOptions {
+  hostname: string;
+  port: number;
+  endpoint: string;
+  data: any;
+}
+
+export const hukkup = (
+  {hostname, port, endpoint, data}: HukkupOptions,
+  callback: (err: any, data: any) => void,
+) => {
+  const body = JSON.stringify(data)
+  const opts = {
+    hostname,
+    port,
+    path: endpoint,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(body),
+    },
+  };
+
+  const req = request(opts, res => {
+    let body = '';
+    res.setEncoding('utf8');
+    res.on('data', chunk => {
+      body += chunk;
+    });
+    res.on('end', () => {
+      callback(null, body);
+    });
+  });
+
+  req.on('error', err => {
+    callback(err, null);
+  });
+
+  req.write(body);
+  req.end();
+};
